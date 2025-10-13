c180607e3ace9197e9e4133141ebc3d3f58685ee
RodriguezBravoSergi <136020404+Thekillerjoker@users.noreply.github.com>
2025-10-13 17:16:27 +0200

    sd

---
+++ Apunts-ASIX/Introducio-Programacio/Muntanyes_Rodriguez_Bravo_Sergi.py
@@ -0,0 +1,10 @@
+numero1 = int(input())
+numero2 = int(input())
+numero3 = int(input())
+numero4 = int(input())
+numero5 = int(input())
+
+if numero2 > numero1  and numero3 < numero2 and numero4 > numero3 and numero5 < numero4 :
+    print("SI")
+else :
+    print("NO")
