34174ca7776aa157883e7a65365c469f09ebcfa8
RodriguezBravoSergi <136020404+Thekillerjoker@users.noreply.github.com>
2025-10-13 16:59:59 +0200

    d

---
+++ Apunts-ASIX/Introducio-Programacio/Iron-4_Rodriguez_Bravo_Sergi.py
@@ -0,0 +1,17 @@
+tecla = input()
+if tecla == "Q" :
+    print("Skill 1")
+elif tecla == "W" :
+    print("Skill 2")
+elif tecla == "E" :
+    print("Skill 3")
+elif tecla == "R" :
+    print("Ultimate")
+elif tecla == "B" :
+    print("Recall")
+elif tecla == "D" :
+    print("Bronzes never use summoners")
+elif tecla == "F" :
+    print("Bronzes never use summoners")
+else :
+    print("Error")
