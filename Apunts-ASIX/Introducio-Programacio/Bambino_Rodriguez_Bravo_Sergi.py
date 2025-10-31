c21325fef09168dda9889a9742aed188717e77b8
RodriguezBravoSergi <sergirodriguezbravo15@gmail.com>
2025-10-31 11:27:02 +0100

    adsas

---
+++ Apunts-ASIX/Introducio-Programacio/Bambino_Rodriguez_Bravo_Sergi.py
@@ -0,0 +1,12 @@
+A = int(input())
+A >= 18
+B = int(input())
+B >= 18
+C = int(input())
+C >= 18
+N = int(input())
+N >= 18
+if N != A and N != B and N != C :
+    print("SI")
+else :
+    print("NO")
