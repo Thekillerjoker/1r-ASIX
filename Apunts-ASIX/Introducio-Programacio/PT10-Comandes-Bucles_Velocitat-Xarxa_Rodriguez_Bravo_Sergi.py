63e60de63743c6dc2cbc89280aade15535b7ef5d
RodriguezBravoSergi <sergirodriguezbravo15@gmail.com>
2025-12-12 10:32:04 +0100

    ddff

--- Apunts-ASIX/Introducio-Programacio/PT10-Comandes-Bucles_Velocitat-Xarxa_Rodriguez_Bravo_Sergi.py
+++ Apunts-ASIX/Introducio-Programacio/PT10-Comandes-Bucles_Velocitat-Xarxa_Rodriguez_Bravo_Sergi.py
@@ -5,9 +5,9 @@ for i in range(3):
     temps1 = time.time()
     os.system("wget -q -o /dev/null https://ash-speed.hetzner.com/100MB.bin ")
     temps2 = time.time()
-    durada = temps2 -temps1
+    durada = temps2 - temps1
     moment = datetime.now()
     data = moment.strftime("%Y-%m-%d")
     hora = moment.strftime("%H:%M:%S")
-    print(f"{data}, {hora},{durada:.2f}")
+    print(f"{data}, {hora},{durada:.2f}") # con .2f te obliga a que el valor de la durada te la mueste con dos decimales
     time.sleep(10)
