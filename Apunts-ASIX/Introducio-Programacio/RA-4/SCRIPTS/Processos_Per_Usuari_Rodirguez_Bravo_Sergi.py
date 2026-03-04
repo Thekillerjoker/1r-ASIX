9cb9c9030edfa8af7bd3866f9b5baf945455cd43
RodriguezBravoSergi <sergirodriguezbravo15@gmail.com>
2026-03-04 08:20:02 +0100

    dd

---
+++ Apunts-ASIX/Introducio-Programacio/RA-4/SCRIPTS/Processos_Per_Usuari_Rodirguez_Bravo_Sergi.py
@@ -0,0 +1,24 @@
+import os
+import time
+programa = True
+while programa == True:
+    sortida = os.popen(f'ps aux ')
+    linies = sortida.readlines()
+    processos = {}
+    for linea in linies[1:]:
+        camps = linea.split()
+        if len(camps) > 0:
+            usuari = camps[0]
+            if usuari not in processos:
+                processos[usuari] = 1
+            else:
+                processos[usuari] += 1
+    usuari_demanat = input('Introdueix el nom del usuari desitjat, per sortir del programa escriu FI: ')
+    if usuari_demanat in processos:
+        print(f'Nombre de processos del usuari {usuari_demanat}: {processos[usuari_demanat]}')
+    elif usuari_demanat == 'FI' or usuari_demanat == 'Fi' or usuari_demanat == 'fi':
+        print('sortint del programa.......')
+        time.sleep(2)
+        programa = False
+    else:
+        print('Aquest usuari no existeix') 
