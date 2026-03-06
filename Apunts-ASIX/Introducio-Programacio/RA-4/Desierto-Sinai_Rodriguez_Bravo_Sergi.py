e1dcd81c59e068871b07378670fbb9bb5b99a46b
RodriguezBravoSergi <sergirodriguezbravo15@gmail.com>
2026-03-06 11:16:41 +0100

    dd

--- Apunts-ASIX/Introducio-Programacio/RA-4/Desierto-Sinai_Rodriguez_Bravo_Sergi.py
+++ Apunts-ASIX/Introducio-Programacio/RA-4/Desierto-Sinai_Rodriguez_Bravo_Sergi.py
@@ -1,7 +1,14 @@
-Cassos = int(input())
+Cassos = int(input('Introdueix el numero de casos:' ))
 for x in range(Cassos):
-    k = int(input())
+    k = int(input('Introdueix el numero de linies:' ))
     vots = {}
     for i in range(k):
-        mapa = input()
-        
+        mapa = input('Introdueix el nom del mapa:' )
+        vots[mapa] = vots.get(mapa, 0) + 1
+    max_vots = 0
+    mapa_guanyador = ""
+    for mapa, vots_del_mapa in vots.items():
+        if vots_del_mapa > max_vots:
+            max_vots = vots_del_mapa
+            mapa_guanyador = mapa
+    print(mapa_guanyador)
