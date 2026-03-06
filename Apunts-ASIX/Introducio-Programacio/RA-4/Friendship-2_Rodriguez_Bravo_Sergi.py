9e6ee6383318bb712f97eaf2222a51304b2104a7
RodriguezBravoSergi <sergirodriguezbravo15@gmail.com>
2026-03-06 11:50:17 +0100

    adad

--- Apunts-ASIX/Introducio-Programacio/RA-4/Friendship-2_Rodriguez_Bravo_Sergi.py
+++ Apunts-ASIX/Introducio-Programacio/RA-4/Friendship-2_Rodriguez_Bravo_Sergi.py
@@ -1,13 +1,13 @@
-Casos = int(input())
+Casos = int(input('Introdueix el nombre de Cassos:' ))
 resultats = []
 for i in range(Casos):
-    linies = int(input())
+    linies = int(input('Introdueix el numero de línies:' ))
     amics = {}
     for x in range(linies - 1):
         a, b = input().split()
         amics[a] = b
         amics[b] = a
-    consulta = input()
+    consulta = input('Introdueix el nombre a consultar:' )
     resultats.append(amics[consulta])
 for r in resultats:
     print(r)
