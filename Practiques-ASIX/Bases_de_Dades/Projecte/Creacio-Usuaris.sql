0b469059608748c084c6284453c84b773617ec29
RodriguezBravoSergi <sergirodriguezbravo15@gmail.com>
2026-04-10 09:11:00 +0200

    ad

--- Practiques-ASIX/Bases_de_Dades/Projecte/Creacio-Usuaris.sql
+++ Practiques-ASIX/Bases_de_Dades/Projecte/Creacio-Usuaris.sql
@@ -1,11 +1,11 @@
-CREATE USER 'admin_f1'@'localhost' IDENTIFIED BY 'Puertollano0101!';
-GRANT ALL PRIVILEGES ON Formula1.* TO 'admin_f1'@'localhost';
+CREATE USER 'admin_f1'@'%' IDENTIFIED BY 'Puertollano0101!';
+GRANT ALL PRIVILEGES ON Formula1.* TO 'admin_f1'@'%';
 FLUSH PRIVILEGES;
 
-CREATE USER 'programador_f1'@'localhost' IDENTIFIED BY 'Programdor1234!';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER ON Formula1.* TO 'programador_f1'@'localhost';
+CREATE USER 'programador_f1'@'%' IDENTIFIED BY 'Programdor1234!';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER ON Formula1.* TO 'programador_f1'@'%';
 FLUSH PRIVILEGES;
 
-CREATE USER 'usuarir_f1'@'localhost' IDENTIFIED BY 'UsuariSegura123!';
-GRANT SELECT ON Formula1.* TO 'usuarir_f1'@'localhost';
+CREATE USER 'usuarir_f1'@'%' IDENTIFIED BY 'UsuariSegura123!';
+GRANT SELECT ON Formula1.* TO 'usuarir_f1'@'%';
 FLUSH PRIVILEGES;
