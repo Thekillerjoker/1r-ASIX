d2e30274bfd51c63b571608e85b2881ce72ffb7b
RodriguezBravoSergi <sergirodriguezbravo15@gmail.com>
2026-04-10 10:54:25 +0200

    ads

---
+++ Practiques-ASIX/Bases_de_Dades/Projecte/Index.sql
@@ -0,0 +1,10 @@
+CREATE INDEX idx_resultats_pilot ON RESULTATS(pilot_id);
+
+
+CREATE INDEX idx_resultats_cursa ON RESULTATS(cursa_id);
+
+
+CREATE INDEX idx_gp_circuit ON GP(circuit_id);
+
+
+CREATE INDEX idx_contractes_pilot ON CONTRACTES(pilot_id);
