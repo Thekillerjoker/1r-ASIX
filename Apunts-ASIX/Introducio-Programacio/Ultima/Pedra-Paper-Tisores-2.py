2ed10ce9ef0f6a241eea7f6b7a89ed57917ce6f0
RodriguezBravoSergi <sergirodriguezbravo15@gmail.com>
2026-04-13 09:50:44 +0200

    asddsa

--- Apunts-ASIX/Introducio-Programacio/Ultima/Pedra-Paper-Tisores-2.py
+++ Apunts-ASIX/Introducio-Programacio/Ultima/Pedra-Paper-Tisores-2.py
@@ -1,4 +1,3 @@
-ERROR = -1
 EMPAT = 0
 PEDRA = 1
 PAPER = 2
@@ -11,23 +10,23 @@ def qui_guanya(tria_jugador1, tria_jugador2):
    if ((tria_jugador1 == PEDRA and tria_jugador2 == PAPER) or
            (tria_jugador1 == PAPER and tria_jugador2 == TISORA) or
            (tria_jugador1 == TISORA and tria_jugador2 == PEDRA)):
-       guanya = 2
+       guanya = "Jugador2"
    elif ((tria_jugador2 == PEDRA and tria_jugador1 == PAPER) or
          (tria_jugador2 == PAPER and tria_jugador1 == TISORA) or
          (tria_jugador2 == TISORA and tria_jugador1 == PEDRA)):
-       guanya = 1
+       guanya = "Jugador1"
    elif (tria_jugador1 == tria_jugador2 and
          tria_jugador1 in [PEDRA, PAPER, TISORA]):
        return EMPAT
    else:
-       guanya = ERROR
+       guanya = "ERROR"
    return guanya
 
 
 
 
-tria1 = int(input())
-tria2 = int(input())
+tria_jugador1 = int(input())
+tria_jugador2 = int(input())
 
-
-(...)
+resultat = qui_guanya(tria_jugador1, tria_jugador2)
+print(resultat)
