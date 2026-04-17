f527e831dffed48a8e3bdda4a9d399b72c2cecb0
RodriguezBravoSergi <sergirodriguezbravo15@gmail.com>
2026-04-17 08:53:40 +0200

    adad

--- Apunts-ASIX/Introducio-Programacio/Ultima/Basketmania-2.py
+++ Apunts-ASIX/Introducio-Programacio/Ultima/Basketmania-2.py
@@ -1,25 +1,24 @@
 def tractar_cas():
-   for i in tractar_cas:
-       k = int(input())
-       local = 0
-       visitant = 0
-       for i in k:
-           equip, punts = input().split()
-           punts = int(punts)
-           if equip == 'L':
-               local += punts
-           else:
-               visitant += punts
-       if local > visitant :
-           guanya = "L"
-       elif visitant > local :
-           guanya = 'V'
-       else:
-           guanya = 'E'
- 
-
-
+    k = int(input())
+    local = 0
+    visitant = 0
+    
+    for i in range(k):
+        equip, punts = input().split()
+        punts = int(punts)
+        
+        if equip == 'L':
+            local += punts
+        else:
+            visitant += punts
+    if local > visitant:
+        print("L", local, visitant)
+    elif visitant > local:
+        print("V", local, visitant)
+    else:
+        print("E", local, visitant)
+        
 casosPendents = int(input())
 while casosPendents > 0:
-   tractar_cas()
-   casosPendents -= 1
+    tractar_cas()
+    casosPendents -= 1
